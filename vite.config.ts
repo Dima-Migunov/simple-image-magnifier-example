0378fe809e2c1fb730c5e5f5391863ed7c417d4c
Dmitri Migunov <dmitri.migunov@gmail.com>
2024-02-15 20:08:07 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/simple-image-magnifier/',
+    base: '/simple-image-magnifier-example/',
     mode: 'production',
     plugins: [react()],
 })
